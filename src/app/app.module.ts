39608636f27c7656c542ef1302a3d253aa3ec170
Jonathas Germano <jonathas.germano@hotmail.com>
2021-05-30 01:51:18 -0300

    NGRX Devtools setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { StoreModule } from '@ngrx/store'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { AuthModule } from 'src/app/auth/auth.module'
+import { environment } from 'src/environments/environment'
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +15,10 @@ import { AuthModule } from 'src/app/auth/auth.module'
     AppRoutingModule,
     AuthModule,
     StoreModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
